be4b056f518d1ac92f87fbc47fd62dd06a4082d7
unknown <cdevogel25@gmail.com>
2023-05-11 09:10:56 -0500

    put everything in a class, added quickstart functions

--- spacetraders.py
+++ spacetraders.py
@@ -1,16 +1,45 @@
 import requests
 import json
 
-URL = 'https://api.spacetraders.io/v2/my/agent'
-# headers = {"Authorization": "Bearer {token}"}
+class sTrader:
 
-def getToken():
-    with open('token.json') as f:
-        data = json.loads(f.read())
-    return data['token']
+    URL = 'https://api.spacetraders.io/{}'
 
-token = getToken()
-headers = {"Authorization": "Bearer {}".format(token)}
+    def __init__(self, token):
+        self.token = self.getToken(token)
+        self.authHeaders = {"Authorization": "Bearer {}".format(self.token)}
+        self.agent = self.getAgent()
 
-r = requests.get(url=URL, headers=headers)
-print(r.text)
+    def getToken(self, token):
+        with open(token) as f:
+            data = json.loads(f.read())
+        return data['token']
+    
+    def getAgent(self):
+        agentURL = self.URL.format('v2/my/agent')
+        r = requests.get(url=agentURL, headers=self.authHeaders)
+        agentData = r.json()
+
+        return agentData
+
+    def getLocation(self):
+        headquarters = self.agent['data']['headquarters'].split('-')
+
+        location = {
+            'sector': headquarters[0],
+            'system': '-'.join(headquarters[0:2]),
+            'waypoint': '-'.join(headquarters[0:3])
+        }
+
+        locationURL = 'v2/systems/{systemSymbol}/waypoints/{waypointSymbol}'.format(
+            systemSymbol=location['system'],
+            waypointSymbol=location['waypoint']
+        )
+
+        r = requests.get(url=self.URL.format(locationURL), headers=self.authHeaders)
+        locationData = r.json()
+        return locationData
+
+trader = sTrader('./token/token.json')
+
+print(trader.getLocation())
